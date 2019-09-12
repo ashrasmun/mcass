0e010e423360c67034505981209fa9f60d491ae8
ashrasmun <ashrasmun@gmail.com>
2019-09-12 20:55:24 +0200

    Moves additional functions to mcass_utils module

--- backup.py
+++ backup.py
@@ -6,20 +6,7 @@
 
 import sys
 import os
-import shutil
-import pwd
-import re
 import mcass_utils
-
-# Credits to https://stackoverflow.com/a/35091558/2059351
-# It throws on wrong indices... I will write this myself, sooner or later >.>
-def replacenth(string, sub, wanted, n):
-    where = [m.start() for m in re.finditer(sub, string)][n - 1]
-    before = string[:where]
-    after = string[where:]
-    after = after.replace(sub, wanted)
-    newString = before + after
-    return newString
 
 def check_arguments():
     if len(sys.argv) < 2:
@@ -49,30 +36,7 @@ def check_if_file_exists(file_name):
                 os.linesep)
         sys.exit(1)
 
-# WARN: This is Linux specific, for obvious reasons.
-# If there's ever a need to implement this for other platforms,
-# please consult the link below:
-# https://stackoverflow.com/questions/4579908/cross-platform-splitting-of-path-in-python/4580931#4580931
-def prepare_path_in_bank(bank_location, file_path, user_name):
-    final_directory_branch = \
-        bank_location +      \
-        replacenth(file_path, '/' + user_name, "", 1) 
-
-    return final_directory_branch
-
-# NOTE: This works only for scripts located in subdirectory of 
-# $HOME directory. I'm too tired to fix it now tho. It would be nice
-# to take care of it after introducing unit tests for these scripts,
-# as some of the functions start to get out of hand.
-def recreate_directory_branch_in_bank(file_name):
-    dir_branch = prepare_path_in_bank(            \
-            mcass_utils.get_bank_location(),      \
-            mcass_utils.absolute_path(file_name), \
-            pwd.getpwuid(os.getuid()).pw_name)
-
-    if dir_branch.find("home") != -1:
-        warn_about_home = True
-
+def create_dir_branch(dir_branch):
     try:
         os.makedirs(dir_branch)
         print(                        \
@@ -85,11 +49,6 @@ def recreate_directory_branch_in_bank(file_name):
     except:
         print("Unspecified error encountered!");
 
-    return (dir_branch, warn_about_home)
-
-def copy_file_with_permissions(script_file_name, to):
-    shutil.copy2(script_file_name, to)
-
 def main():
     check_arguments()
     make_sure_bank_exists()
@@ -101,12 +60,14 @@ def main():
         check_if_file_exists(file_name)
 
         (directory_branch, was_home_used) = \
-                recreate_directory_branch_in_bank(file_name)
+                mcass_utils.recreate_directory_branch_in_bank(file_name)
+    
+        create_dir_branch(directory_branch)
 
         if was_home_used:
             should_warn_about_home = True
 
-        copy_file_with_permissions(file_name, directory_branch)
+        mcass_utils.copy_file_with_permissions(file_name, directory_branch)
 
     if should_warn_about_home:
         print("WARNING:"                                            + \
--- mcass_utils.py
+++ mcass_utils.py
@@ -3,10 +3,52 @@
 # Utils used in more than one MCaSS scripts
 
 import os
+import pwd
+import shutil
+import re
 
 def absolute_path(file_name):
     return os.path.dirname(os.path.realpath(file_name))
 
 def get_bank_location():
     return os.path.join(absolute_path(__file__), "bank")
+
+# Credits to https://stackoverflow.com/a/35091558/2059351
+# It throws on wrong indices... I will write this myself, sooner or later >.>
+def replacenth(string, sub, wanted, n):
+    where = [m.start() for m in re.finditer(sub, string)][n - 1]
+    before = string[:where]
+    after = string[where:]
+    after = after.replace(sub, wanted)
+    newString = before + after
+    return newString
+
+# WARN: This is Linux specific, for obvious reasons.
+# If there's ever a need to implement this for other platforms,
+# please consult the link below:
+# https://stackoverflow.com/questions/4579908/cross-platform-splitting-of-path-in-python/4580931#4580931
+def prepare_path_in_bank(bank_location, file_path, user_name):
+    final_directory_branch = \
+        bank_location +      \
+        replacenth(file_path, '/' + user_name, "", 1) 
+
+    return final_directory_branch
+
+# NOTE: This works only for scripts located in subdirectory of 
+# $HOME directory. I'm too tired to fix it now tho. It would be nice
+# to take care of it after introducing unit tests for these scripts,
+# as some of the functions start to get out of hand.
+def recreate_directory_branch_in_bank(file_name):
+    dir_branch = prepare_path_in_bank( \
+            get_bank_location(),       \
+            absolute_path(file_name),  \
+            pwd.getpwuid(os.getuid()).pw_name)
+
+    if dir_branch.find("home") != -1:
+        warn_about_home = True
+
+    return (dir_branch, warn_about_home)
+
+def copy_file_with_permissions(script_file_name, to):
+    shutil.copy2(script_file_name, to)
 
